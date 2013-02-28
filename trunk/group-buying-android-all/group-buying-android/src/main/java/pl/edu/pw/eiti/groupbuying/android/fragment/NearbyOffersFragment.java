950b4e7065b9312d3bd9c6c3dcd7c16d17ad444b
zawadz88@gmail.com <zawadz88@gmail.com@809fbc1d-1fbc-9334-4625-0d325503d8e5>
2013-02-28 19:00:48 +0000

    dodanie ładnego scrollowania mapy

--- trunk/group-buying-android-all/group-buying-android/src/main/java/pl/edu/pw/eiti/groupbuying/android/fragment/NearbyOffersFragment.java
+++ trunk/group-buying-android-all/group-buying-android/src/main/java/pl/edu/pw/eiti/groupbuying/android/fragment/NearbyOffersFragment.java
@@ -1,46 +1,75 @@
 package pl.edu.pw.eiti.groupbuying.android.fragment;
 
 import pl.edu.pw.eiti.groupbuying.android.R;
-
-import com.androidquery.AQuery;
-import com.google.android.gms.maps.SupportMapFragment;
-
+import pl.edu.pw.eiti.groupbuying.android.view.MapHoldingRelativeLayout;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.LinearLayout.LayoutParams;
-import android.widget.TextView;
+import android.widget.RelativeLayout;
 
-public final class NearbyOffersFragment extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
-    public static SupportMapFragment newInstance(String content) {
-        //NearbyOffersFragment fragment = new NearbyOffersFragment();
-        
+public final class NearbyOffersFragment extends SupportMapFragment {
 
-        return new SupportMapFragment();
-    }
+	public static NearbyOffersFragment newInstance(String content) {
+		NearbyOffersFragment fragment = new NearbyOffersFragment();
+		return fragment;
+	}
 
-    private String mContent = "???";
+	private GoogleMap googleMap;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setUpMapIfNeeded();
+	}
 
-/*    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    	final View rootView = inflater.inflate(R.layout.fragment_nearby_offers,	container, false);
-		AQuery aq = new AQuery(getActivity(), rootView);
+	private void setUpMapIfNeeded() {
+		if (googleMap == null) {
+			googleMap = getMap();
+			if (googleMap != null) {
+				setUpMap();
+			}
+		}
+	}
 
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		final View rootView = inflater.inflate(R.layout.fragment_nearby_offers,
+				container, false);
+		final View mapFragmentLayout = super.onCreateView(inflater, container,
+				savedInstanceState);
+		MapHoldingRelativeLayout mapLayout = (MapHoldingRelativeLayout) rootView
+				.findViewById(R.id.mapLayout);
+		mapLayout.addView(mapFragmentLayout);
 		return rootView;
-    }*/
+	}
+	@Override
+	public void onResume() {
+		super.onResume();
+		setUpMapIfNeeded();
+	}
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-    }
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+	}
+
+	private void setUpMap() {
+		googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+		googleMap.getUiSettings().setZoomControlsEnabled(true);
+		googleMap.getUiSettings().setAllGesturesEnabled(true);
+		CameraPosition cameraPosition = new CameraPosition.Builder()
+				.target(new LatLng(41.474856, -88.056928))
+				.zoom(14.5f)
+				.build();
+		googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+	}
 }
