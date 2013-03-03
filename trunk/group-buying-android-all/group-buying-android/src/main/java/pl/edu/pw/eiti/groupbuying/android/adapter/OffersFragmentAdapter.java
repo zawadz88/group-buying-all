88745268d22e0e80b84b2cc9e91ffda758cf8885
zawadz88@gmail.com <zawadz88@gmail.com@809fbc1d-1fbc-9334-4625-0d325503d8e5>
2013-03-03 13:02:17 +0000

    dodanie licencji

--- trunk/group-buying-android-all/group-buying-android/src/main/java/pl/edu/pw/eiti/groupbuying/android/adapter/OffersFragmentAdapter.java
+++ trunk/group-buying-android-all/group-buying-android/src/main/java/pl/edu/pw/eiti/groupbuying/android/adapter/OffersFragmentAdapter.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Piotr Zawadzki.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Piotr Zawadzki - initial API and implementation
+ ******************************************************************************/
 package pl.edu.pw.eiti.groupbuying.android.adapter;
 
 import pl.edu.pw.eiti.groupbuying.android.fragment.CityOffersFragment;
