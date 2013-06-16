849c509206c6c1dc9c1cd94e1a6ff66786935f69
zawadz88@gmail.com <zawadz88@gmail.com@809fbc1d-1fbc-9334-4625-0d325503d8e5>
2013-06-16 14:30:03 +0000

    obsługa wykorzystywania kuponów

--- trunk/group-buying-all/group-buying-partner-rest/src/main/java/pl/edu/pw/eiti/groupbuying/partner/rest/service/impl/CouponServiceImpl.java
+++ trunk/group-buying-all/group-buying-partner-rest/src/main/java/pl/edu/pw/eiti/groupbuying/partner/rest/service/impl/CouponServiceImpl.java
@@ -1,10 +1,18 @@
 package pl.edu.pw.eiti.groupbuying.partner.rest.service.impl;
 
+import java.util.Date;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import pl.edu.pw.eiti.groupbuying.core.dao.CouponDAO;
+import pl.edu.pw.eiti.groupbuying.core.domain.Coupon;
+import pl.edu.pw.eiti.groupbuying.core.domain.Coupon.CouponState;
+import pl.edu.pw.eiti.groupbuying.partner.rest.exception.BadRequestException;
+import pl.edu.pw.eiti.groupbuying.partner.rest.exception.UnauthorizedException;
+import pl.edu.pw.eiti.groupbuying.partner.rest.model.ApiError.ErrorCode;
+import pl.edu.pw.eiti.groupbuying.partner.rest.model.ClaimResponse;
 import pl.edu.pw.eiti.groupbuying.partner.rest.service.CouponService;
 
 @Service("couponService")
@@ -14,5 +22,37 @@ public class CouponServiceImpl implements CouponService {
 
 	@Autowired
 	private CouponDAO couponDAO;
+
+
+	@Override
+	public ClaimResponse claimCoupon(int couponId, String securityKey, String username) {
+		Coupon coupon = couponDAO.getCoupon(couponId);
+		if(coupon == null) {
+			throw new BadRequestException("Coupon with ID: '" + couponId + "' not found.", ErrorCode.COUPON_NOT_FOUND);
+		}
+		if(!coupon.getSecurityKey().equals(securityKey)) {
+			throw new BadRequestException("Invalid security key.", ErrorCode.INVALID_SECURITY_KEY);
+		}
+		if(!coupon.getOffer().getSeller().getEmail().equals(username)) {
+			throw new UnauthorizedException("Coupon '" + couponId + "' belongs to another partner.", ErrorCode.UNAUTHORIZED);
+		}
+		ClaimResponse response = null;
+		switch (coupon.getCouponState()) {
+		case BOUGHT:
+			response = ClaimResponse.CLAIMED;
+			coupon.setUseDate(new Date());
+			coupon.setCouponState(CouponState.REDEEMED);
+			couponDAO.updateCoupon(coupon);
+			break;
+		case EXPIRED:
+			response = ClaimResponse.EXPIRED;
+			break;
+		case REDEEMED:
+			response = ClaimResponse.ALREADY_USED;
+			break;
+		}
+		
+		return response;
+	}
 
 }
