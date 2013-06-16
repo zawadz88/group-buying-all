0b2ca79b711dd7c778a2c6f37b6bf4e8f101fc08
zawadz88@gmail.com <zawadz88@gmail.com@809fbc1d-1fbc-9334-4625-0d325503d8e5>
2013-06-16 14:30:03 +0000

    obsługa wykorzystywania kuponów

--- group-buying-all/group-buying-partner-rest/src/main/java/pl/edu/pw/eiti/groupbuying/partner/rest/service/CouponService.java
+++ group-buying-all/group-buying-partner-rest/src/main/java/pl/edu/pw/eiti/groupbuying/partner/rest/service/CouponService.java
@@ -1,11 +1,8 @@
 package pl.edu.pw.eiti.groupbuying.partner.rest.service;
 
-import java.util.List;
-
-import pl.edu.pw.eiti.groupbuying.core.domain.Client;
-import pl.edu.pw.eiti.groupbuying.core.domain.Offer;
-import pl.edu.pw.eiti.groupbuying.core.dto.CouponDTO;
+import pl.edu.pw.eiti.groupbuying.partner.rest.model.ClaimResponse;
 
 public interface CouponService {
-	
+
+	ClaimResponse claimCoupon(int couponId, String securityKey, String username);
 }
